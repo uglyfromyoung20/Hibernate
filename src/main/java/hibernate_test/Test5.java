5764e9833131ca895e120eae56f192165188768f
altyshkin <29455komandor@mail.ru>
2022-02-06 12:55:50 +0400

    Удаление пользователя из бд

---
+++ src/main/java/hibernate_test/Test5.java
@@ -0,0 +1,28 @@
+package hibernate_test;
+
+import hibernate_test.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test5 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+        try {
+            Session session = factory.getCurrentSession();
+            session.beginTransaction();
+            Employee employee = session.get(Employee.class , 1);
+            session.delete(employee);
+
+            session.getTransaction().commit();
+
+            System.out.println("Done");
+        }
+        finally {
+            factory.close();
+        }
+    }
+}
